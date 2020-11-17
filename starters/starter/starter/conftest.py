dd219f39431ec1a8295d8e3818d9cb043ce80f90
Themba <thembelanimahlangu@outlook.com>
2020-11-17 10:24:44 +0100

    init

---
+++ starters/starter/starter/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from starter.users.models import User
+from starter.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
